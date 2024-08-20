7718fef0be5d8f8bce43c08b78d01d8b80fe6b03
isaque <isaquejosafa52@gmail.com>
2024-08-19 23:09:08 -0300

    Arrumando ortografia!

--- Exercios1LetraC.py
+++ Exercios1LetraC.py
@@ -1,3 +1,3 @@
 valor_Em_Real = 3657.38
-valor_Em_Dola = valor_Em_Real / 5.75 # Taca cambio lógica 1 dola vale 5,75 então é só dividir montante em REAL e dividir pela taxa cambio '5,75'
+valor_Em_Dola = valor_Em_Real / 5.75 # Taxa de cambio... A lógica é, 1 dola vale 5,75. Então é só dividir montante REAL pela taxa cambio '5,75'.
 print(f'Valor em dola: {valor_Em_Dola:.2f}')
--- Exercios1LetraD.py
+++ Exercios1LetraD.py
@@ -1,6 +1,6 @@
-#Bom como Fernando não completou a questão eu vou completar pro mim mesmo.
+#Bom, como Fernando não completou a questão. Eu mesmo vou completar o restante :).
 
-valor_Em_Real = input('Escreva o valor em Real: ')      # input é string então não valor numerio ainda
+valor_Em_Real = input('Escreva o valor em Real: ') # input é string, então não é valor numerico ainda!
 valor_Em_Real = float(valor_Em_Real)
 valor_Da_Taxa_De_Cambio = input('Escreva a taxa de cambio do dola: ')
 valor_Da_Taxa_De_Cambio = float(valor_Da_Taxa_De_Cambio)
@@ -8,4 +8,4 @@ valor_Final_Converdito = valor_Em_Real / valor_Da_Taxa_De_Cambio
 print('')
 print(f'Valor covertido é: {valor_Final_Converdito:.2f}')
 
-# Esse algoritimo é bem falido... Não tem lógica de verificação de pontos ou seja liso :)
+# Esse algoritmo é bem falido... Não tem lógica de verificação de pontos float ou Caracteres... Entre outra palavras é bem liso :)
